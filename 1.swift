5c9be4028e3cd7fd565bda36eee36f7eea894bd5
Enddle <Enddle@users.noreply.github.com>
2018-05-26 11:09:42 +0800

    add relativeDepth
    possible to calculate all 14 points

--- 1.swift
+++ 1.swift
@@ -51,23 +51,26 @@ extension Dictionary where Value: Equatable {
 
 
 let points: [Point] = [  // all points
+    
     Point(3, [2, 1]),
     Point(2, [2, 3]),
     Point(1, [3, 4]),
     Point(1, [4, 2]),
     Point(2, [4, 0]),
-
+    
     //  Using the points in the bottom-left 5 x 5 boxes for testing
     
-//    Point(1, [7, 0]),
-//    Point(3, [6, 2]),
-//    Point(2, [6, 4]),
-//    Point(1, [6, 6]),
-//    Point(2, [7, 7]),
-//    Point(3, [4, 6]),
-//    Point(2, [2, 7]),
-//    Point(1, [0, 7]),
-//    Point(1, [1, 5]),
+    Point(1, [7, 0]),
+    Point(3, [6, 2]),
+    Point(2, [6, 4]),
+    Point(1, [6, 6]),
+    Point(2, [7, 7]),
+    Point(3, [4, 6]),
+    Point(2, [2, 7]),
+    Point(1, [0, 7]),
+    Point(1, [1, 5]),
+    
+    //  With the relativeDepth setting, it's possible to calculate all points
 ]
 
 var map = [Int: Point]()  // put points in map with id
@@ -102,6 +105,12 @@ var indis = 0
 var intime = 0.0
 
 
+/**
+ Search points farther than nearest point by this distance
+ */
+var relativeDepth: Int = 0
+
+
 func loop(_ t: Int, _ seq: [Int], _ pos: [Int], dis: Int) {  // #4
     
     if cal >= maxCal {  // terminate when get to maximum calculate times
@@ -115,42 +124,83 @@ func loop(_ t: Int, _ seq: [Int], _ pos: [Int], dis: Int) {  // #4
         return
     }
     
+    var minmap:[[Int]] = []
+    
     for (pointId, point) in map {
-        
-        // get looping variables
         
         inseq = seq
         inpos = pos
-        indis = dis
         
         if inseq.contains(pointId) {  // route already passed this point
             continue
         }
         
-        // move to this point
+        let mindis = point.distance(inpos)
         
-        inseq.append(pointId)
-        indis += point.distance(inpos)
-        inpos = point.pos
-        
-        if t == points.count - 2 {  // if only one route left
+        if minmap.count != 0 {
             
+            let minans = mindis - minmap[0][1]  // distance from nearest point to this point
+            
+            if minans < -relativeDepth {  // very near, replace minmap by this point
+                
+                minmap = [[pointId, mindis]]
+                
+            } else if -relativeDepth <= minans && minans < 0 {  // near, insert in the beginning
+                
+                minmap.insert([pointId, mindis], at: 0)
+                minmap = minmap.filter { $0[1] <= (mindis + relativeDepth) }  // filtered far points
+                
+            } else if minans <= relativeDepth {  // far, append to minmap
+                
+                minmap.append([pointId, mindis])
+                
+            }
+            
+            // (very far, do nothing)
+            
+        } else {  // if minmap is empty, append this point
+            
+            minmap.append([pointId, mindis])
+        }
+        
+    }
+    
+    for pointIdAndDis in minmap {
+
+        // get looping variables
+
+        inseq = seq
+        inpos = pos
+        indis = dis
+
+        if inseq.contains(pointIdAndDis[0]) {  // route already passed this point
+            continue
+        }
+
+        // move to this point
+
+        inseq.append(pointIdAndDis[0])
+        indis += pointIdAndDis[1]
+        inpos = (map[pointIdAndDis[0]]?.pos)!
+
+        if t == points.count - 2 {  // if only one route left
+
             // using id sums to calculate last point id
             let lastId = (1...points.count-1).map{$0}.reduce(0, +) - inseq.reduce(0, +)
-            
+
             // move to last point
-            
+
             inseq.append(lastId)
             indis += (map[lastId]?.distance(inpos))!
             inpos = (map[lastId]?.pos)!
         }
-        
+
         loop(t + 1, inseq, inpos, dis: indis)  // entering next loop
     }
 }
 
 
-func loop(_ t: Int, _ seq: [Int], _ pos: [Int], time: Double, _ speed: Int = 1) {  // #5
+func loop(_ t: Int, _ seq: [Int], _ pos: [Int], time: Double, speed: Int = 1) {  // #5, similar to 4
     
     if cal >= maxCal {
         return
@@ -163,17 +213,59 @@ func loop(_ t: Int, _ seq: [Int], _ pos: [Int], time: Double, _ speed: Int = 1) 
         return
     }
     
+    var minmap:[[Int]] = []
+    
     for (pointId, point) in map {
         
         inseq = seq
         inpos = pos
-        intime = time
         
         if inseq.contains(pointId) {
             continue
         }
         
-        inseq.append(pointId)
+        let mindis = point.distance(inpos)
+        
+        if minmap.count != 0 {
+            
+            
+            let minans = mindis - minmap[0][1]
+            
+            if minans < -relativeDepth {
+                
+                minmap = [[pointId, mindis]]
+                
+            } else if -relativeDepth <= minans && minans < 0 {
+                
+                minmap.insert([pointId, mindis], at: 0)
+                minmap = minmap.filter { $0[1] <= (mindis + relativeDepth) }
+                
+            } else if minans <= relativeDepth {
+                
+                minmap.append([pointId, mindis])
+                
+            }
+            
+        } else {
+            
+            minmap.append([pointId, mindis])
+        }
+        
+    }
+    
+    for pointId in minmap {
+        
+        inseq = seq
+        inpos = pos
+        intime = time
+        
+        if inseq.contains(pointId[0]) {
+            continue
+        }
+        
+        let point = (map[pointId[0]])!
+        
+        inseq.append(pointId[0])
         intime += point.time(inpos, speed)
         inpos = point.pos
         
@@ -186,7 +278,7 @@ func loop(_ t: Int, _ seq: [Int], _ pos: [Int], time: Double, _ speed: Int = 1) 
             inpos = (map[lastId]?.pos)!
         }
         
-        loop(t + 1, inseq, inpos, time: intime, point.val)
+        loop(t + 1, inseq, inpos, time: intime, speed: point.val)
     }
 }
 
@@ -238,17 +330,19 @@ func showResults(_ dic : Dictionary<Int, Double>) {  // #5
 //_________________________
 
 
-let maxCal = 200  // limiting the max counts of result calculated
+let maxCal = 10000  // limiting the max counts of result calculated
+
+relativeDepth = 0  // adjust relativeDepth
 
 // for #4
 
-//loop(0, inseq, inpos, dis: indis)
-//showResults(diss)
+loop(0, inseq, inpos, dis: indis)
+showResults(diss)
 
 // for #5
 
-loop(0, inseq, inpos, time: intime)
-showResults(times)
+//loop(0, inseq, inpos, time: intime)
+//showResults(times)
 
 
 
